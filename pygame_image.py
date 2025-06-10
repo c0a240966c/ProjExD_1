5a35f1df482d90f2dcc1f168065fda58051c2098
Ryosei <c0a240966c@edu.teu.ac.jp>
2025-06-10 15:58:20 +0900

    練習9残像

--- pygame_image.py
+++ pygame_image.py
@@ -19,12 +19,14 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        x = tmr%3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600,-0])
+        screen.blit(bg_img, [-x+3200,-0])
 
-        screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg_img2, [-tmr+1600,-0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
-        tmr += 1        
+        tmr += 10      
         clock.tick(200)
 
         
